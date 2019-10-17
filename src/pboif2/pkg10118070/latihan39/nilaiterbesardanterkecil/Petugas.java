efd3180d76a5c1a2b585313f32437de00a90f488
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-10-17 23:22:38 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan39/nilaiterbesardanterkecil/Petugas.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan39.nilaiterbesardanterkecil;
+
+/**
+ *
+ * @author Ailyasha
+ * Nama : Aufa Ilyasha
+ * Nim : 10118070
+ * Kelas : IF-2
+ * Nama kelas : Pemrograman Berbasis OO
+ */
+public class Petugas {
+    private String nama;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }    
+    
+}
